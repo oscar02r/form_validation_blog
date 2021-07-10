22e3e462bf73a63b2e12ab508489a750b74da67a
Oscary <oscary.ramirez@gmail.com>
2021-07-10 12:22:51 -0400

    Backgroud porpure, circles and logo ready

--- lib/src/page/login_page.dart
+++ lib/src/page/login_page.dart
@@ -15,7 +15,7 @@ class LoginPage extends StatelessWidget{
 
  Widget _crearFondo(BuildContext context) {
       final size = MediaQuery.of(context).size;
-       return Container(
+       final fondoMorado = Container(
                height: size.height * 0.40,
                width: double.infinity,
           decoration:BoxDecoration(
@@ -26,6 +26,43 @@ class LoginPage extends StatelessWidget{
               ]
             )
           ),
+       );
+
+
+       final crearCirculo = Container(
+         height: 100.0,
+         width: 100.0,
+         decoration: BoxDecoration(
+           borderRadius: BorderRadius.circular(100.0),
+           color: Color.fromRGBO(255, 255, 255, 0.05)
+         ),
+       );
+
+
+       return Stack(
+         children: [
+           fondoMorado,
+           Positioned(child: crearCirculo, top: 90.0, left: 30.0),
+           Positioned(child: crearCirculo, top: -40.0, right:-30.0),
+           Positioned(child: crearCirculo, bottom: -50.0, left: -10.0),
+           Positioned(child: crearCirculo, bottom: 120, right: 20.0),
+           Positioned(child: crearCirculo, bottom: -50, right:- 20.0),
+           Container(
+             padding: EdgeInsets.only(top: 80.0),
+             child: Column(
+
+               children: [
+                 Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0,),
+                 SizedBox(height: 10.0, width: double.infinity,),
+                 Text('Oscary Ramirez', style: TextStyle(
+                     fontSize: 25.0,
+                     color: Colors.white
+                 ),
+                 )
+               ],
+             ),
+           )
+         ],
        );
   }
 }
