f44d8f75ccd41e734758e45e6416a48067e8d794
Oscary <oscary.ramirez@gmail.com>
2021-07-11 15:25:48 -0400

    Adding utils validation and state full widget to form

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:form_blog/src/bloc/provider.dart';
+import 'package:form_blog/src/page/producto_page.dart';
 
 class HomePage extends StatelessWidget{
   static final String routeName = 'home' ;
@@ -12,15 +13,18 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Home page')
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('Email: ${bloc.email}'),
-          Divider(),
-          Text('Password: ${bloc.password}'),
-        ],
-      ),
+      body: Container(),
+      floatingActionButton:  _crearBoton(context),
+    );
+  }
+
+  Widget _crearBoton( BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: Theme.of(context).primaryColor,
+      child: Icon(Icons.add),
+      onPressed: (){
+        Navigator.pushNamed(context, ProductoPage.routeName);
+      },
     );
   }
 }
--- lib/src/page/producto_page.dart
+++ lib/src/page/producto_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:form_blog/src/utils/utils.dart' as utils ;
 
-class ProductoPage extends StatelessWidget {
+class ProductoPage extends StatefulWidget {
 static final routeName = 'producto';
+
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +27,73 @@ static final routeName = 'producto';
           )
         ],
       ),
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Form(
+          key:formKey,
+          child: Column(
+            children: [
+              _crearNombre(context),
+              _crearPrecio(),
+               _crearBoton(context)
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+ Widget _crearNombre(BuildContext context) {
+    return TextFormField(
+
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Producto'
+      ),
+      cursorColor: Theme.of(context).primaryColor,
+      validator: ( value){
+         if(value!.length < 3){
+             return 'El nombre del producto deber mayor que 3.';
+         }else{
+           return null;
+         }
+      },
+    );
+ }
+
+Widget  _crearPrecio() {
+    return TextFormField(
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(
+        labelText: 'Precio'
+      ),
+      validator: (value){
+        if(   utils.isNumeric(value!)){
+          return null;
+        }else{
+          return 'Solo numeros';
+        }
+
+      }
+    );
+}
+
+ Widget _crearBoton(BuildContext context ) {
+    return ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          primary: Theme.of(context).primaryColor,
+          onPrimary: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0)
+          )
+        ),
+        icon: Icon(Icons.save),
+        label: Text('Guardar'),
+      onPressed:_submit,
+    );
+ }
+
+  void _submit() {
+    formKey.currentState?.validate();
   }
 }
