9c5a11d86eba95e466a98eb678349b57490980c8
Oscary <oscary.ramirez@gmail.com>
2021-07-11 10:44:50 -0400

    Creating singleton in Provider

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -4,7 +4,16 @@ import 'package:form_blog/src/bloc/login_bloc.dart';
 export  'package:form_blog/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget{
-  Provider({Key ? key,  child}) : super(key:key , child:child );
+  static  Provider? _instancia;
+
+  factory Provider({Key ? key,  child}){
+
+        if(_instancia == null){
+          _instancia = new Provider._internal(child: child);
+        }
+        return _instancia!;
+  }
+  Provider._internal({Key ? key,  child}) : super(key:key , child:child );
 
   final loginBloc = LoginBloc();
 
