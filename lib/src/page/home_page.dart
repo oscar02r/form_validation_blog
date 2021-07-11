5b3f04f09aa55433144ced3f4e863ab947043503
Oscary <oscary.ramirez@gmail.com>
2021-07-11 19:35:58 -0400

    Creating cargarProductos ready

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:form_blog/src/bloc/provider.dart';
+import 'package:form_blog/src/models/producto_model.dart';
 import 'package:form_blog/src/page/producto_page.dart';
+import 'package:form_blog/src/providers/producto_providers.dart';
 
 class HomePage extends StatelessWidget{
   static final String routeName = 'home' ;
+  final productoProvider = ProductoProvider();
+
   @override
   Widget build(BuildContext context) {
 
@@ -13,11 +17,20 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Home page')
       ),
-      body: Container(),
+      body: _crearListado(),
       floatingActionButton:  _crearBoton(context),
     );
   }
 
+ Widget _crearListado(){
+     return FutureBuilder(
+         future: productoProvider.cargarProductos() ,
+         builder: (context, AsyncSnapshot<List<ProductoModel>> snapshot){
+          // if(snapshot.hasData) print();
+           return Container();
+         }
+     );
+  }
   Widget _crearBoton( BuildContext context) {
     return FloatingActionButton(
       backgroundColor: Theme.of(context).primaryColor,
--- lib/src/providers/producto_providers.dart
+++ lib/src/providers/producto_providers.dart
@@ -3,16 +3,30 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:form_blog/src/models/producto_model.dart';
 
-class ProductoProvider{
+class ProductoProvider {
   final String _url = 'https://flutter-varios-11539-default-rtdb.firebaseio.com';
 
-  Future<bool> crearProducto( ProductoModel producto) async{
-    final url = Uri.parse('$_url/productos.json') ;
+  Future<bool> crearProducto(ProductoModel producto) async {
+    final url = Uri.parse('$_url/productos.json');
 
-    final resp = await http.post(url,body: productoModelToJson(producto) );
-    final decodeData =  json.decode(resp.body);
+    final resp = await http.post(url, body: productoModelToJson(producto));
+    final decodeData = json.decode(resp.body);
     return true;
   }
 
+  Future <List<ProductoModel>> cargarProductos() async {
+    final url = Uri.parse('$_url/productos.json');
+    final resp = await http.get(url);
+    final List<ProductoModel> productos = [];
+    final Map<String, dynamic> decodeData = json.decode(resp.body);
+    decodeData.forEach((id, prod) {
+        ProductoModel prodTemp = ProductoModel.fromJson(prod);
+        prodTemp.id = id;
+        productos.add(prodTemp);
+
+    });
+
+    return productos;
+  }
 
 }
