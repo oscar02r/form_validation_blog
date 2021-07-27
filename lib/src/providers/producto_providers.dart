e5bb4e89f61e6de89ba5b548f74c24de7e704742
Oscary <oscary.ramirez@gmail.com>
2021-07-26 22:25:30 -0400

    Creating upload image provider

--- lib/src/providers/producto_providers.dart
+++ lib/src/providers/producto_providers.dart
@@ -1,6 +1,10 @@
 
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:http/http.dart' as http;
+import 'package:mime_type/mime_type.dart';
 import 'package:form_blog/src/models/producto_model.dart';
 
 class ProductoProvider {
@@ -43,5 +47,27 @@ class ProductoProvider {
     return 1;
   }
 
+  Future<String> uploadImage(File image) async{
+    final url = Uri.parse('https://api.cloudinary.com/v1_1/dtfvbtscn/image/upload?upload_preset=jbajnsrc');
+    final mimeType = mime(image.path)!.split('/'); //image/jpeg
 
+    final imageUploadRequest = http.MultipartRequest('POST', url);
+
+    final file = await http.MultipartFile.fromPath(
+        'file',
+        image.path,
+      contentType: MediaType(mimeType[0], mimeType[1])
+    );
+
+    imageUploadRequest.files.add(file);
+    final streamResponse = await imageUploadRequest.send();
+    final resp = await  http.Response.fromStream(streamResponse);
+    if(resp.statusCode !=200 && resp.statusCode !=201){
+      print('Algo salio mal');
+      print ('${resp.body}');
+      return '';
+    }
+    final dataResp = json.decode(resp.body);
+    return dataResp['secure_url'];
+  }
 }
