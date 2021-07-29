ddeee1be736f9ed5c3d8039caf727448af525822
Oscary <oscary.ramirez@gmail.com>
2021-07-29 18:57:02 -0400

    Creatring login in provider user

--- lib/src/providers/usuario_provider.dart
+++ lib/src/providers/usuario_provider.dart
@@ -3,9 +3,31 @@ import 'dart:convert';
 import'package:http/http.dart' as http;
 
 class UsuarioProvider{
+
   final String _firebaseToken ='AIzaSyC0yZKZTfGDWYzMUmplhhHdxzwWktpFMZk';
 
-  Future nuevoUsuario(String email , String password) async {
+  Future<Map<String, dynamic>> login(String email, String password) async{
+    final authData = {
+      'email':email,
+      'password':password,
+      'returnSecureToken':true
+    };
+    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$_firebaseToken');
+    final resp = await http.post(
+        url,
+        body: json.encode(authData)
+    );
+
+    Map<String, dynamic> decodedResp = json.decode(resp.body);
+    print(decodedResp);
+    if(decodedResp.containsKey('idToken')){
+      return {'ok':true, 'token': decodedResp['idToken'] };
+    }else{
+      return {'ok':false, 'mensaje': decodedResp['error']['message']};
+    }
+  }
+
+  Future<Map<String, dynamic>> nuevoUsuario(String email , String password) async {
        final authData = {
          'email':email,
          'password':password,
@@ -18,7 +40,7 @@ class UsuarioProvider{
        );
 
        Map<String, dynamic> decodedResp = json.decode(resp.body);
-
+          print(decodedResp);
        if(decodedResp.containsKey('idToken')){
          return {'ok':true, 'token': decodedResp['idToken'] };
        }else{
